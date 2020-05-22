aef9800613eea9d30a72b6db26c0aff62c370e5e
Komstuch <komstuchxbox@gmail.com>
2020-05-22 21:11:39 +0200

    Rising water in level 3

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip("Game units per second")]
+    [SerializeField] float scrollSpeed = 1f;
+    void Update()
+    {
+        float yMove = scrollSpeed * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+    }
+}
